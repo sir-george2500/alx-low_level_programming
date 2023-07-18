ecbe984ec2caa303b753588672a8b60329da2fce
George S. Mulbah <gsmulbah2500@gmail.com>
2023-07-18 16:37:54 -0700

    Solve task 0 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ * is written by George S Mulbah II
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
