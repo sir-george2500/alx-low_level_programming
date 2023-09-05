3d144673a52e895d56112fe24da720ab8ab96408
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-05 01:56:22 +0000

    added print_type

---
+++ 0x15-file_io/print_abi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_abi - Prints the ABI version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF ABI version.
+ */
+void print_abi(unsigned char *e_ident)
+{
+	printf(" ABI Version: %d\n",
+		e_ident[EI_ABIVERSION]);
+}
+
