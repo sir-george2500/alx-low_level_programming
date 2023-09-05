3dce736b84e27c842c84b6d02d52d798435f1c4e
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-05 01:59:34 +0000

    added close_elf

---
+++ 0x15-file_io/close_elf.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * close_elf - Closes an ELF file.
+ * @elf: The file descriptor of the ELF file.
+ *
+ * Description: If the file cannot be closed - exit code 98.
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", elf);
+		exit(98);
+	}
+}
