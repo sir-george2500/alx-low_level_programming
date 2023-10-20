75991ebaa9c3c8bddf0cdefb14eba6e9c1e1caee
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-10-20 10:58:33 +0000

    solve advanced problem

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer (non-zero).
+ *
+ * Return: The result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Computes the modulus of two integers.
+ * @a: The first integer.
+ * @b: The second integer (non-zero).
+ *
+ * Return: The modulus of a divided by b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
